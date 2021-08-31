71776fb1e5b7d8cc52c13a1090ad9859699f4c8d
Kayla Dombrosky <kdombroskyy@live.com>
2021-08-31 17:15:15 -0400

    file structure

---
+++ src/components/About/About/index.js
@@ -0,0 +1,21 @@
+import React from 'react'; 
+import coverImage from "../../assets/cover/cover-image.jpg";
+
+function About() {
+    // Return JSX - JavaScript XML
+    // React.createElement("the html element you wish to create", props, 
+    // remaining children(any child elements between the opening and closing tag of that element))
+    
+    // This is the behind the scenes equivilant of what is being returned below
+    // React.createElement("section", {}, [
+    // React.createElement("h1", {}, "Who am I?"),
+    // ])
+    return (
+        <section className="my-5">
+            <h1 id="about">Who am I?</h1>
+            <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+        </section>
+    );
+}
+
+export default About;
